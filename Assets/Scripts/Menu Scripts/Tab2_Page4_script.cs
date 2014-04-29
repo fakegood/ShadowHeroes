391130400d8c194349f963e81cba76e5fa971f58
fakegood <jiahao@repulze.com>
2014-04-29 18:57:11 +0900

    view unit detail update

--- Assets/Scripts/Menu Scripts/Tab2_Page4_script.cs
+++ Assets/Scripts/Menu Scripts/Tab2_Page4_script.cs
@@ -4,16 +4,13 @@ using System.Collections;
 public class Tab2_Page4_script : SubPageHandler {
 	
 	public UILabel someText;
+	private int selectedIndex = -1;
 
 	// Use this for initialization
 	void Start () {
 		if(csObj == null) return;
 
-		someText.text = base.csObj.characterProperties[base.parent.currentSelectedDeckNum-1].cardName;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		selectedIndex = base.parent.currentSelectedDeckNum-1;
+		someText.text = base.csObj.characterProperties[selectedIndex].cardName;
 	}
 }
